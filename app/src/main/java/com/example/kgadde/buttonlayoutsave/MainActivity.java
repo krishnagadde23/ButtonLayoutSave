8e630eb0572e43b8b0245f68b83213c5584e2e20
krishnagadde23 <krishnagadde23@gmail.com>
2015-09-21 14:47:12 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kgadde/buttonlayoutsave/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.kgadde.buttonlayoutsave;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    int b1press;
+    int b2press;
+    int b3press;
+    int b4press;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b1press++;
+                Toast.makeText(MainActivity.this, " B1 Pressed " + b1press + " times", Toast.LENGTH_SHORT).show();
+                Log.i("onCreate","ButtonPressed");
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b2press++;
+                Toast.makeText(MainActivity.this, "B2 Pressed " + b2press + " times", Toast.LENGTH_SHORT).show();
+                Log.i("onCreate", "ButtonPressed");
+            }
+        });
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b3press++;
+                Toast.makeText(MainActivity.this, "B3 Pressed " + b3press + " times", Toast.LENGTH_SHORT).show();
+                Log.i("onCreate", "ButtonPressed");
+            }
+        });
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b4press++;
+                Toast.makeText(MainActivity.this, "B4 Pressed " + b4press + " times", Toast.LENGTH_SHORT).show();
+                Log.i("onCreate", "ButtonPressed");
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
